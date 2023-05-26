287e0d2ae1e941987bac111e9f535a4ac81d3ca5
Leonid Vegera <leonid.vegera@gmail.com>
2023-05-26 18:05:54 +0300

    ChatPage added

--- src/ChatsPage.jsx
+++ src/ChatsPage.jsx
@@ -1,5 +1,13 @@
-const ChatsPage = () => {
-  return <div className="background">chats...</div>;
+import {MultiChatSocket, MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced';
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic('961e361d-4e9f-47d4-a49f-370d7cde436a', props.user.username, props.user.secret)
+  return (
+    <div>
+      <MultiChatSocket {...chatProps}/>
+      <MultiChatWindow {...chatProps} style={{height: '100%'}}/>
+    </div>
+  )
 }
 
 export default ChatsPage;
