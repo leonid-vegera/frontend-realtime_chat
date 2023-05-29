606cb36e15c48d11b18f94c27f714997ab8e8960
Leonid Vegera <leonid.vegera@gmail.com>
2023-05-29 21:06:27 +0300

    Advanced Scheme

--- src/ChatsPage.jsx
+++ src/ChatsPage.jsx
@@ -3,9 +3,9 @@ import {MultiChatSocket, MultiChatWindow, useMultiChatLogic} from 'react-chat-en
 const ChatsPage = (props) => {
   const chatProps = useMultiChatLogic('71f977b1-32e2-4530-9b9a-985129adee2b', props.user.username, props.user.secret)
   return (
-    <div>
+    <div style={{height: '100vh'}}>
       <MultiChatSocket {...chatProps}/>
-      <MultiChatWindow {...chatProps} style={{height: '100vh'}}/>
+      <MultiChatWindow {...chatProps} style={{height: '100%'}}/>
     </div>
   )
 }
